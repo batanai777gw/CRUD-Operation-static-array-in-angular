b3122e99a549428e284d75ec2bca291c242ad955
batanai777gw <batanai178@gmail.com>
2023-07-18 18:39:56 +0200

    bug-fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,43 +5,85 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent {
 
-  StudentName = "";
+  studentName = "";
 
-  selectedStudent = null;
-  //an array of students
-  StudentArray : string[] = ["John","James","Junior", "Batanai"];
+  selectedStudentIndex = 0;
+
+  //array of students
+  studentArray: string[] = ["John","James","Junior", "Batanai"];
 
   constructor() {}
 
   ngOnInit() {}
 
-  //method to add a student to the student array
-  AddStudent(){
-    this.StudentArray.push(this.StudentName);
-    this.StudentName = "";
-    alert("Student added successfully")
+  //adding a student to an array
+  addStudent(){
+    this.studentArray.push(this.studentName);
+    this.studentName = "";
+    alert("Student added successfully");
   }
 
-    //method for deleting student name
+  //deleting a student from array
 
-  deleteStudent(i: any) {
-   if(confirm("Are you sure you want to delete")){
-    this.StudentArray.splice(i,1);
-   }
-  }
-
-    //method for selecting student name
-
-  selectStudent(i: any){
-    this.selectStudent = i;
-    this.StudentName = this.StudentArray[i];
+  deleteStudent(i: any){
+    if(confirm("Are you sure you want to delete!")){
+      this.studentArray.splice(i,1);
     }
-  
-    //method for editing student name
-  EditStudent(){
-    this.StudentArray[this.selectedStudent] = this.selectedStudent;
-    this.selectedStudent = null;
+  }
+
+  //selecting a student
+  selectStudent(i: any){
+    this.selectedStudentIndex = i;
+    this.studentName = this.studentArray[i];
+  }
+
+  editStudent(){
+    this.studentArray[this.selectedStudentIndex] = this.studentName;
+    this.selectedStudentIndex = 0;
+    this.studentName = "";
   }
 }
+
+
+// export class HomeComponent {
+
+//   StudentName = "";
+
+//   selectedStudent = null;
+//   //an array of students
+//   StudentArray : string[] = ["John","James","Junior", "Batanai"];
+
+//   constructor() {}
+
+//   ngOnInit() {}
+
+//   //method to add a student to the student array
+//   AddStudent(){
+//     this.StudentArray.push(this.StudentName);
+//     this.StudentName = "";
+//     alert("Student added successfully")
+//   }
+
+//     //method for deleting student name
+
+//   deleteStudent(i: any) {
+//    if(confirm("Are you sure you want to delete")){
+//     this.StudentArray.splice(i,1);
+//    }
+//   }
+
+//     //method for selecting student name
+
+//   selectStudent(i: any){
+//     this.selectStudent = i;
+//     this.StudentName = this.StudentArray[i];
+//     }
+  
+//   EditStudent(){
+//     this.StudentArray[this.selectedStudent] = this.selectedStudent;
+//     this.selectedStudent = null;
+//   }
+// }
