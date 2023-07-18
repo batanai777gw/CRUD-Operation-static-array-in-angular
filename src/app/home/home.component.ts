6ff0d4f2f3676d03d7b369cbf285d25c75387c27
batanai777gw <batanai178@gmail.com>
2023-07-18 07:30:14 +0200

    finished-project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,9 @@ export class HomeComponent {
 
   StudentName = "";
 
+  selectedStudent = null;
   //an array of students
-  StudentArray : string[] = [];
+  StudentArray : string[] = ["student1", "student2", "student3" ];
 
   constructor() {}
 
@@ -19,6 +20,23 @@ export class HomeComponent {
   //method to add a student to the student array
   AddStudent(){
     this.StudentArray.push(this.StudentName);
-    this.StudentName = ""
+    this.StudentName = "";
+    alert("Student added successfully")
+  }
+
+  deleteStudent(i: any) {
+   if(confirm("Are you sure you want to delete")){
+    this.StudentArray.splice(i,1);
+   }
+  }
+
+  selectStudent(i: any){
+    this.selectStudent = i;
+    this.StudentName = this.StudentArray[i];
+    }
+  
+  EditStudent(){
+    this.StudentArray[this.selectedStudent] = this.StudentName;
+    this.selectedStudent = null;
   }
 }
